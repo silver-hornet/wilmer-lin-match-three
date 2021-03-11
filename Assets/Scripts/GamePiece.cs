a7eb4724f325577f13a667db90db8817edc07aa4
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-11 07:44:39 -0500

    Completed Lesson 9

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -7,19 +7,71 @@ public class GamePiece : MonoBehaviour
     public int xIndex;
     public int yIndex;
 
+    bool m_isMoving = false; // to prevent any buggy movement between frames, particuarly if you try to double tap
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move((int)transform.position.x - 1, (int)transform.position.y, 0.5f);
+        }
     }
 
     public void SetCoord(int x, int y)
     {
         xIndex = x;
         yIndex = y;
+    }
+
+    public void Move(int destX, int destY, float timeToMove)
+    {
+        if (!m_isMoving)
+        {
+            StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
+        }
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        Vector3 startPosition = transform.position;
+
+        bool reachedDestination = false;
+
+        float elapsedTime = 0f;
+
+        m_isMoving = true;
+
+        while (!reachedDestination)
+        {
+            // if we are close enough to destination
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoord((int)destination.x, (int)destination.y);
+                break;
+            }
+
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0f, 1f); // although don't really need to use Mathf.Clamp, since Lerp already has a clamp built-in
+
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+            // wait until next frame
+            yield return null;
+        }
+
+        m_isMoving = false;
     }
 }
