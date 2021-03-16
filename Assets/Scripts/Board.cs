7dfafb0df045f921bc00a7dc8087ea1f5fdc1db5
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-15 22:06:08 -0400

    Completed Lesson 19

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -212,11 +212,8 @@ public class Board : MonoBehaviour
             else
             {
                 yield return new WaitForSeconds(swapTime);
-                ClearPieceAt(clickedPieceMatches);
-                ClearPieceAt(targetPieceMatches);
 
-                CollapseColumn(clickedPieceMatches);
-                CollapseColumn(targetPieceMatches);
+                ClearAndRefillBoard(clickedPieceMatches.Union(targetPieceMatches).ToList());
             }
         }
     }
@@ -356,6 +353,18 @@ public class Board : MonoBehaviour
         return combinedMatches;
     }
 
+    List<GamePiece> FindMatchesAt(List<GamePiece> gamePieces, int minLength = 3)
+    {
+        List<GamePiece> matches = new List<GamePiece>();
+
+        foreach (GamePiece piece in gamePieces)
+        {
+            matches = matches.Union(FindMatchesAt(piece.xIndex, piece.yIndex, minLength)).ToList();
+        }
+
+        return matches;
+    }
+
     void HighlightTileOff(int x, int y)
     {
         SpriteRenderer spriteRenderer = m_allTiles[x, y].GetComponent<SpriteRenderer>();
@@ -393,6 +402,17 @@ public class Board : MonoBehaviour
         }
     }
 
+    void HighlightPieces(List<GamePiece> gamePieces)
+    {
+        foreach (GamePiece piece in gamePieces)
+        {
+            if (piece != null)
+            {
+                HighlightTileOn(piece.xIndex, piece.yIndex, piece.GetComponent<SpriteRenderer>().color);
+            }
+        }
+    }
+
     void ClearPieceAt(int x, int y)
     {
         GamePiece pieceToClear = m_allGamePieces[x, y];
@@ -421,7 +441,10 @@ public class Board : MonoBehaviour
     {
         foreach (GamePiece piece in gamePieces)
         {
-            ClearPieceAt(piece.xIndex, piece.yIndex);
+            if (piece != null)
+            {
+                ClearPieceAt(piece.xIndex, piece.yIndex);
+            }
         }
     }
 
@@ -484,5 +507,56 @@ public class Board : MonoBehaviour
         }
 
         return columns;
+    }
+
+    void ClearAndRefillBoard(List<GamePiece> gamePieces)
+    {
+        StartCoroutine(ClearAndRefillBoardRoutine(gamePieces));
+    }
+
+    IEnumerator ClearAndRefillBoardRoutine(List<GamePiece> gamePieces)
+    {
+        // clear and collapse
+
+        StartCoroutine(ClearAndCollapseRoutine(gamePieces));
+        yield return null;
+
+        // refill
+    }
+
+    IEnumerator ClearAndCollapseRoutine(List<GamePiece> gamePieces)
+    {
+        List<GamePiece> movingPieces = new List<GamePiece>();
+        List<GamePiece> matches = new List<GamePiece>();
+
+        HighlightPieces(gamePieces);
+
+        yield return new WaitForSeconds(0.25f);
+
+        bool isFinished = false;
+
+        while (!isFinished)
+        {
+            ClearPieceAt(gamePieces);
+
+            yield return new WaitForSeconds(0.25f);
+
+            movingPieces = CollapseColumn(gamePieces);
+
+            yield return new WaitForSeconds(0.25f);
+
+            matches = FindMatchesAt(movingPieces);
+
+            if (matches.Count == 0)
+            {
+                isFinished = true;
+                break;
+            }
+            else
+            {
+                yield return StartCoroutine(ClearAndCollapseRoutine(matches));
+            }
+        }
+        yield return null;
     }
 }
