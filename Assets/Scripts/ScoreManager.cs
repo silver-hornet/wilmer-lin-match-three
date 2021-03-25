c7c500720d4b0f657b7d7c818cd71d59aaaed52c
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-25 07:44:05 -0400

    Completed Lesson 42

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : Singleton<ScoreManager>
+{
+    int m_currentScore = 0;
+    int m_counterValue = 0;
+    int m_increment = 5;
+
+    public Text scoreText;
+
+    void Start()
+    {
+        UpdateScoreText(m_currentScore);
+    }
+
+    public void UpdateScoreText(int scoreValue)
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = scoreValue.ToString();
+        }
+    }
+
+    public void AddScore(int value)
+    {
+        m_currentScore += value;
+        StartCoroutine(CountScoreRoutine());
+    }
+
+    IEnumerator CountScoreRoutine()
+    {
+        int iterations = 0;
+
+        while (m_counterValue < m_currentScore && iterations < 100000) // Less than 100000 to prevent an infinite loop
+        {
+            m_counterValue += m_increment;
+            UpdateScoreText(m_counterValue);
+            iterations++;
+            yield return null; // wait a frame
+        }
+
+        m_counterValue = m_currentScore;
+        UpdateScoreText(m_currentScore);
+    }
+}
