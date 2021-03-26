0b5e5b0f5f058f1bd50282e09ea8648674a20c60
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-26 08:15:17 -0400

    Completed Lesson 48

---
+++ Assets/Scripts/MessageWindow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectXformMover))] // To ensure that the message window game object always has the RectXformMover attached to it
+public class MessageWindow : MonoBehaviour
+{
+    public Image messageIcon;
+    public Text messageText;
+    public Text buttonText;
+
+    public void ShowMessage(Sprite sprite = null, string message = "", string buttonMsg = "start")
+    {
+        if (messageIcon != null)
+        {
+            messageIcon.sprite = sprite;
+        }
+
+        if (messageText != null)
+        {
+            messageText.text = message;
+        }
+
+        if (buttonText != null)
+        {
+            buttonText.text = buttonMsg;
+        }
+    }
+}
---
+++ Assets/Scripts/RectXformMover.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class RectXformMover : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3 onscreenPosition;
+    public Vector3 endPosition;
+
+    public float timeToMove = 1f;
+
+    RectTransform m_rectXform;
+    bool m_isMoving = false;
+
+    void Awake()
+    {
+        m_rectXform = GetComponent<RectTransform>();
+    }
+
+    void Move(Vector3 startPos, Vector3 endPos, float timeToMove)
+    {
+        StartCoroutine(MoveRoutine(startPos, endPos, timeToMove));
+    }
+
+    IEnumerator MoveRoutine(Vector3 startPos, Vector3 endPos, float timeToMove)
+    {
+        if (m_rectXform != null)
+        {
+            m_rectXform.anchoredPosition = startPos;
+        }
+
+        bool reachedDestination = false;
+        float elapsedTime = 0f;
+        m_isMoving = true;
+
+        while (!reachedDestination)
+        {
+            if (Vector3.Distance (m_rectXform.anchoredPosition, endPos) < 0.01f)
+            {
+                reachedDestination = true;
+                break;
+            }
+
+            elapsedTime += Time.deltaTime; // incrementing by the time the last frame took
+
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0f, 1f);
+            t = t * t * t * (t * (t * 6 - 15) + 10); // Smootherstep formula
+
+            if (m_rectXform != null)
+            {
+                m_rectXform.anchoredPosition = Vector3.Lerp(startPos, endPos, t);
+            }
+
+            yield return null; // waits a frame
+        }
+
+        m_isMoving = false;
+    }
+
+    public void MoveOn()
+    {
+        Move(startPosition, onscreenPosition, timeToMove);
+    }
+
+    public void MoveOff()
+    {
+        Move(onscreenPosition, endPosition, timeToMove);
+    }
+}
